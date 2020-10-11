3e724035b64d0dd4dc269509240b373a3f377503
Benitha250 <benithauwase05@gmail.com>
2020-10-11 10:05:14 -0400

    add further edit

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,14 @@ function getNumber() {
 }
 function mushrooms() {
     var mushroom = 0;
-    var addMushroom = document.getElementById("topping");
+    var addMushroom = document.getElementById("mush",);
     if (addMushroom.checked === true) {
         mushroom = 300;
     }
     return parseInt(mushroom);
 }
 
-function calctotalPrice(event) {
+function calctotalPrice(e) {
     event.preventDefault();
     var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
     
@@ -39,7 +39,7 @@ $(document).ready(function () {
         var number = $("input#number").val();
         var location = $("input#location").val();
 
-        alert("Hello " + name + ". Thank you for chosing LaNoshery. Your order has been successfuly received and will be delivered to " +  location + ".The delivery will cost 1000 Rwf. We will call you shortly on " + number);
+        alert("Hello " + name + ". Thank you for chosing LaNoshery. Your order has been successfully received and will be delivered to " +  location + ".The delivery will cost 1000 Rwf. We will call you shortly on " + number);
 
     });
     
